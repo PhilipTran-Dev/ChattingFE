e4c82891d290b13a224ae6bcdae93ab70cb0879a
Philip Tran <tnvphuccntt@gmail.com>
2026-09-01 13:53:42 +0700

    feat: setup multi-server axios and update cicd for s3

--- src/Config/AxiosHelper.js
+++ src/Config/AxiosHelper.js
@@ -1,16 +1,18 @@
-// import axios from "axios";
-// // export const baseURL = "http://localhost:8080";
-// export const baseURL = "http://ec2-18-138-228-181.ap-southeast-1.compute.amazonaws.com:8080";
-
-
-// export const HttpClient = axios.create({
-//     baseURL: baseURL,
-// });
-
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_BACKEND_URL || "http://192.168.1.9:8080";
+export const SERVERS = {
+    LOCAL: "http://192.168.1.9:8080",
+    VPS: "http://ec2-18-141-169-207.ap-southeast-1.compute.amazonaws.com:8080",
+    AWS: "http://18.141.169.207",
+};
+
+const ACTIVE_SERVER = import.meta.env.VITE_ACTIVE_SERVER || "AWS"; // "LOCAL" | "VPS" | "AWS"
+
+export const baseURL = SERVERS[ACTIVE_SERVER] || SERVERS.AWS;
 
 export const HttpClient = axios.create({
     baseURL: baseURL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
